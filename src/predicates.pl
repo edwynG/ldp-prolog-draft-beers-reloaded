ed861fff47ae44243fb4690237fc6c53b08fc9d4
Katricia21 <patydanicc@gmail.com>
2025-06-23 21:42:46 -0400

    cambios

--- src/predicates.pl
+++ src/predicates.pl
@@ -1,9 +1,6 @@
-% Modulo para funciones del proyecto
-:- module(predicates, [initialBarrels/3, iSolution/3, addBeer/3, findSolution/3, barrel/3]). 
-
 :- dynamic barrel/3.
 
-% Predicado principal para inicializar barriles
+% Inicializa los barriles con capacidades y volúmenes
 initialBarrels(["A", "B", "C"], [CA, CB, CC], [BA0, BB0, BC0]) :-
     maplist(valid_number, [CA, CB, CC, BA0, BB0, BC0]),
     retractall(barrel(_, _, _)),
@@ -20,72 +17,54 @@ cascade_fix(CA, CB, CC, A, B, C, FA, FB, FC) :-
     ; cascade_fix(CA, CB, CC, NA, NB, NC, FA, FB, FC)
     ).
 
-% - A y C transfieren exceso a B
-% - B transfiere exceso a barril con menos cerveza (A o C)
 fix_once(CA, CB, CC, A0, B0, C0, A2, B2, C2) :-
-    % Desborde de A y C hacia B
     excess(A0, CA, EA, A1),
     excess(C0, CC, EC, C1),
     B1Temp is B0 + EA + EC,
-
-    % Desborde de B hacia el barril con menor cantidad
     excess(B1Temp, CB, EB, B1),
-    ( A1 =< C1 ->
+
+    ( A1 =< C1, CA > 0 ->
         free_space(A1, CA, FA),
         Transfer is min(EB, FA),
         A2 is A1 + Transfer,
         C2 = C1
-    ;
+    ; C1 =< A1, CC > 0 ->
         free_space(C1, CC, FC),
         Transfer is min(EB, FC),
         C2 is C1 + Transfer,
         A2 = A1
+    ;
+        A2 = A1,
+        C2 = C1
     ),
     B2 = B1.
 
-% Calcula el exceso si se pasa de la capacidad, devuelve el valor fijo
 excess(Value, Max, Excess, Fixed) :-
     (Value > Max -> Excess is Value - Max, Fixed is Max ; Excess = 0, Fixed = Value).
 
-% Calcula el espacio libre en un barril
 free_space(Current, Capacity, Free) :-
     Free is Capacity - Current.
 
-% Verifica que sea un número entero no negativo
-valid_number(N) :-
-    integer(N), N >= 0.
+valid_number(N) :- integer(N), N >= 0.
 
-% Predicado para iSolution
-% Verifica si al agregar Beer al Barrel, es posible alcanzar Goal litros en algún barril
 iSolution(Barrel, Beer, Goal) :-
-    % Guardar capacidades y estado actual
     barrel("A", CA, BA),
     barrel("B", CB, BB),
     barrel("C", CC, BC),
+    
+    % Validar si podemos agregar cerveza al barril indicado
+    (Barrel = "A", CA > 0 -> NewBA is BA + Beer, NewBB = BB, NewBC = BC ;
+     Barrel = "C", CC > 0 -> NewBC is BC + Beer, NewBA = BA, NewBB = BB ;
+     fail),
 
-    % Limpiar base de conocimientos
-    retractall(barrel(_, _, _)),
-
-    % Agregar cerveza según el barril destino
+    % Simulación sin tocar la base real
+    cascade_fix(CA, CB, CC, NewBA, NewBB, NewBC, FA, FB, FC),
+    
+    % Evaluar el resultado sin modificar los hechos
     (
-        Barrel = "A" -> NewBA is BA + Beer, NewBB = BB, NewBC = BC ;
-        Barrel = "C" -> NewBC is BC + Beer, NewBA = BA, NewBB = BB ;
-        fail  % solo A o C permiten agregar cerveza
-    ),
-
-    % Manejo de desbordes en cascada
-    cascade_fix(CA, CB, CC, NewBA, NewBB, NewBC, FinalA, FinalB, FinalC),
-
-    % Restaurar nuevo estado corregido
-    assertz(barrel("A", CA, FinalA)),
-    assertz(barrel("B", CB, FinalB)),
-    assertz(barrel("C", CC, FinalC)),
-
-    % Verificar si existe solución en algún barril
-    (
-        FinalA >= Goal ;
-        FinalB >= Goal ;
-        FinalC >= Goal
+        FA >= Goal ;
+        FB >= Goal ;
+        FC >= Goal
     ),
     !.
 
@@ -134,46 +113,43 @@ addBeer(Barrel, Beer, Transfer) :-
 all_solutions(Goal, Result):-
     (barrel(_, _, Beer), Beer >= Goal
     -> Solutions0 = [(0, "N/A")]
-    ; Solutions0 = []
+    ;  Solutions0 = []
     ),
 
-    % 1. Desde A: llenar A
     barrel("A", CapA, CurrA),
+    barrel("B", CapB, CurrB),
+    barrel("C", CapC, CurrC),
+
+    % 1. Desde A: llenar A directamente
     NeedA is Goal - CurrA,
-    (NeedA > 0, CurrA + NeedA =< CapA ->
+    (CapA > 0, NeedA > 0, CurrA + NeedA =< CapA ->
         Solutions1 = [(NeedA, "A")|Solutions0]
     ;   Solutions1 = Solutions0
     ),
 
-    % 2. Desde A: llenar B
-    barrel("B", CapB, CurrB),
-    OverflowA = CapA - CurrA,
+    % 2. Desde A: llenar B (A desborda a B)
+    OverflowA is CapA - CurrA,
     NeedB is Goal - CurrB,
-    
-    (NeedB > 0, CurrB + NeedB =< CapB ->
+    (CapA > 0, CapB > CurrB, NeedB > 0, CurrB + NeedB =< CapB ->
         R is NeedB + OverflowA,
         Solutions2 = [(R, "A")|Solutions1]
     ;   Solutions2 = Solutions1
     ),
-    
-    % 3. Desde A: llenar C (solo si C < A)
-    barrel("C", CapC, CurrC),
+
+    % 3. Desde A: llenar C vía B
     OverflowB is CapB - CurrB,
-    (CurrC =< CapA ->
-        NeedC is Goal - CurrC,
-        (NeedC > 0, CurrC + NeedC =< CapC ->
-            R2 is NeedC + OverflowA + OverflowB,
-            Solutions3 = [(R2, "A")|Solutions2]
-        ;   Solutions3 = Solutions2
-        )
+    (CapA > 0, CapB > CurrB, CapC > CurrC,
+     CurrC =< CapA,
+     NeedC is Goal - CurrC,
+     NeedC > 0, CurrC + NeedC =< CapC ->
+        R2 is NeedC + OverflowA + OverflowB,
+        Solutions3 = [(R2, "A")|Solutions2]
     ;   Solutions3 = Solutions2
     ),
-        
-    % 4. Desde C: llenar C
-    barrel("C", CapC2, CurrC2),
-    OverflowC is CapC2 - CurrC2,
-    NeedC2 is Goal - CurrC2,
-    (NeedC2 > 0, CurrC2 + NeedC2 =< CapC2 ->
+
+    % 4. Desde C: llenar C directamente
+    NeedC2 is Goal - CurrC,
+    (CapC > 0, NeedC2 > 0, CurrC + NeedC2 =< CapC ->
         Solutions4 = [(NeedC2, "C")|Solutions3]
     ;   Solutions4 = Solutions3
     ),
@@ -181,45 +157,49 @@ all_solutions(Goal, Result):-
     % 5. Desde C: llenar B
     barrel("B", CapB2, CurrB2),
     NeedB2 is Goal - CurrB2,
-    (NeedB2 > 0, CurrB2 + NeedB2 =< CapB2 ->
+    OverflowC is CapC - CurrC,
+    (CapC > 0, CapB2 > CurrB2, NeedB2 > 0, CurrB2 + NeedB2 =< CapB2 ->
         R3 is NeedB2 + OverflowC,
         Solutions5 = [(R3, "C")|Solutions4]
     ;   Solutions5 = Solutions4
     ),
 
-    % 6. Desde C: llenar A (solo si A < C)
+    % 6. Desde C: llenar A vía B
     barrel("A", CapA2, CurrA2),
-    (CurrA2 =< CapC2 ->
-        NeedA2 is Goal - CurrA2,
-        (NeedA2 > 0, CurrA2 + NeedA2 =< CapA2 ->
-            R4 is NeedA2 + OverflowC + OverflowB,
-            Solutions6 = [(R4, "C")|Solutions5]
-        ;   Solutions6 = Solutions5
-        )
+    (CapC > 0, CapB > CurrB, CapA2 > CurrA2,
+     CurrA2 =< CapC,
+     NeedA2 is Goal - CurrA2,
+     NeedA2 > 0, CurrA2 + NeedA2 =< CapA2 ->
+        R4 is NeedA2 + OverflowC + OverflowB,
+        Solutions6 = [(R4, "C")|Solutions5]
     ;   Solutions6 = Solutions5
     ),
-    % Filtrar soluciones válidas (ya filtradas arriba)
-    Result = Solutions6.
 
+    Result = Solutions6.
 findSolution(Goal, SolutionType, Result) :-
     number(Goal),
     string(SolutionType),
     var(Result),
     Goal >= 0,
-    (SolutionType = "best"; SolutionType="all"),
+    (SolutionType = "best"; SolutionType = "all"),
+
+    % Verificar si ya se cumple sin agregar cerveza
     ( forall(barrel(_, _, Beer), Beer >= Goal)
-    ->  Result = (0, "N/A")
-    ; 
-        all_solutions(Goal,List),
-        ( SolutionType = "best" ->
-            ( not(List = []) ->
-                sort(List, Sorted), Sorted = [Best|_], Result = Best,!
-            ;   fail
-            )
-        ; SolutionType = "all" ->
-            ( not(List = []) ->
+    -> Result = (0, "N/A"), !
+
+    % Verificar si existe al menos un barril A o C con capacidad > 0
+    ; ( barrel("A", CapA, CurrA), CapA > CurrA
+      ; barrel("C", CapC, CurrC), CapC > CurrC
+      )
+    -> all_solutions(Goal, List),
+       ( SolutionType = "best" ->
+            ( List \= [] ->
+                sort(List, Sorted), Sorted = [Best|_], Result = Best, !
+            ; fail )
+       ; SolutionType = "all" ->
+            ( List \= [] ->
                 sort(List, Sorted), member(Result, Sorted)
-            ;  fail
-            )
-        )
+            ; fail )
+       )
+    ; fail  % No hay forma de agregar cerveza a ningún barril
     ).
